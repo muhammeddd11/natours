be376002b0bbbc20cb67c1b37627b05f0f621204
muhammeddd11 <muhammedfared11@gmail.com>
2024-07-20 19:43:03 +0200

    add parameter pollution prevention , implement rate limit ,setting security headers and add data sanitization to prevent noSQL injection and xss

--- routes/tourRouters.js
+++ routes/tourRouters.js
@@ -20,7 +20,7 @@ router
   .route('/topFiveCheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
 router.route('/tourStats').get(tourController.getTourStats)
-router.route('/').get(authController.protect,tourController.getAllTours).post(tourController.addTour);
+router.route('/').get(tourController.getAllTours).post(tourController.addTour);
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(authController.protect,authController.restrictedTo('admin',"leg-guide"),tourController.deleteTour);
 
   module.exports = router
